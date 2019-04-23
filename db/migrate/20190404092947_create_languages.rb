73fee9847ef1513e48b41912ad0da55215a2a7d3
truong <truong.luong@tech.reapra.sg>
2019-04-23 14:02:36 +0700

    A) Add index for language's code field and activity's name field

--- db/migrate/20190404092947_create_languages.rb
+++ db/migrate/20190404092947_create_languages.rb
@@ -1,9 +1,10 @@
 class CreateLanguages < ActiveRecord::Migration[5.2]
   def change
     create_table :languages do |t|
-      t.string :code, null: false
+      t.string :code, null: false, limit: 25
 
       t.timestamps
     end
+    add_index :languages, :code, :unique => true, :name => 'index_language_code'
   end
 end
--- db/migrate/20190404093855_create_activities.rb
+++ db/migrate/20190404093855_create_activities.rb
@@ -1,9 +1,10 @@
 class CreateActivities < ActiveRecord::Migration[5.2]
   def change
     create_table :activities do |t|
-      t.string :name, null: false
+      t.string :name, null: false, limit: 190
 
       t.timestamps
     end
+    add_index :activities, :name, :unique => true, :name => 'index_activity_name'
   end
 end
